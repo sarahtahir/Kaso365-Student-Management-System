7610ee626da96cedac2fcea130bb5c08c97da0c9
Siti Sarah Mohd Tahir <sarahsiti2000@gmail.com>
2020-08-11 15:26:44 +0800

    repair student list

--- studentInfo/studentList/studentList.php
+++ studentInfo/studentList/studentList.php
@@ -43,7 +43,12 @@
 					</td>'
 					;
 				
-					echo '<td><button type="button" data-toggle="modal" data-target="#view" data-uid="1" class="delete btn btn-info btn-sm"><span class="glyphicon glyphicon glyphicon-eye-open"></span></button></td>
+					echo '<td>';
+				    echo '<form action="viewStudentInfo.php" method="post" >';
+			        echo "<input type='hidden' value='$studentID' name='studentIDView'>";
+					echo '<button type="submit" data-toggle="modal" name="viewStudBtn" data-target="#edit"  class="update btn btn-warning btn-sm"><span class="glyphicon glyphicon-pencil"></span></button></form>
+					</td>'
+					;
 		            <td><button type="button" data-toggle="modal" data-target="#delete" data-uid="1" class="delete btn btn-danger btn-sm"><span class="glyphicon glyphicon-trash"></span></button></td>
 		        </tr>
 		        <tr id="d2"> </tr>
