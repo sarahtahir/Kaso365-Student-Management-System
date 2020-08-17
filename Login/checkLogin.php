ca5e1af74fe2e4280dc233e1ba382a130cf974fa
Siti Sarah Mohd Tahir <sarahsiti2000@gmail.com>
2020-08-17 13:09:44 +0800

    changed path in files

--- Login/checkLogin.php
+++ Login/checkLogin.php
@@ -23,7 +23,7 @@ if($isValidUser)
 else {
 	echo '<div class="w3-container" style="width:80%; margin:0 auto;">';
 	echo "<center><br><br>Wrong Username or Password";
-	echo '<br><br span class="w3-right w3-padding w3-hide-large w3-large"><br><a href="index.html">Try Again?</a></span>';
+	echo '<br><br span class="w3-right w3-padding w3-hide-large w3-large"><br><a href="../index.html">Try Again?</a></span>';
 	echo '</div></center>';
 	}
 ?>
